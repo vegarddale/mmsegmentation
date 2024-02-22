00a24ddc54c6c790b800dca64c34d621b6032d95
Vegard Dale <vegard17@uia.no>
2024-02-22 12:17:42 +0000

    added deformable mscan using deformable sw conv

--- configs/custom/dmscan_potsdam.py
+++ configs/custom/dmscan_potsdam.py
@@ -8,9 +8,11 @@ crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
 model = dict(
     backbone=dict(
-        attn_module="DCNv3KA",
-        kernel_size=[5, 7],
-        pad=[2,3]
+        attn_module="DCNv3_SW_KA",
+        kernel_size=[3, [1, 5], [1, 7], [1, 11]],
+        pad=[2, [0, 3], [0, 5], [0, 10]],
+        embed_dims=[8, 16, 32, 64],
+        mlp_ratios=[4, 4, 4, 4]
     ),
     data_preprocessor=data_preprocessor,
     decode_head=dict(num_classes=6))
--- mmseg/models/utils/dcnv3_ka.py
+++ mmseg/models/utils/dcnv3_ka.py
@@ -29,36 +29,40 @@ class DCNv3KA(BaseModule):
                  offset_scale=1.0,
                  dw_kernel_size=None, # for InternImage-H/G
                  res_post_norm=False, # for InternImage-H/G
-                 center_feature_scale=False): 
+                 center_feature_scale=False,
+                 strip_conv=0): 
         super().__init__()
         self.channels = channels
-        self.core_op = getattr(dcnv3, 'DCNv3')
+        self.core_op = getattr(dcnv3, core_op)
         self.output_proj = nn.Linear(channels, channels)
         self.dw_dcn = self.core_op(
             channels=channels,
-            kernel_size=5,
+            kernel_size=tuple((kernel_size[0], kernel_size[0])),
             stride=1,
-            pad=2,
+            pad=tuple((pad[0], pad[0])),
             dilation=1,
             group=channels,
             offset_scale=offset_scale,
             act_layer=act_layer,
             norm_layer=norm_layer,
             dw_kernel_size=dw_kernel_size, # for InternImage-H/G
-            center_feature_scale=center_feature_scale)
+            center_feature_scale=center_feature_scale,
+            strip_conv=strip_conv)
         
         self.dw_d_dcn = self.core_op(
             channels=channels,
-            kernel_size=7,
+            kernel_size=tuple((kernel_size[1], kernel_size[1])),
             stride=1,
-            pad=3,
+            pad=tuple((pad[1], pad[1])),
             dilation=1,
             group=channels,
             offset_scale=offset_scale,
             act_layer=act_layer,
             norm_layer=norm_layer,
             dw_kernel_size=dw_kernel_size, # for InternImage-H/G
-            center_feature_scale=center_feature_scale)
+            center_feature_scale=center_feature_scale,
+            strip_conv=strip_conv
+        )
 
     def forward(self, x):
         u = x.clone()
@@ -75,4 +79,114 @@ class DCNv3KA(BaseModule):
         self.dw_d_dcn._reset_parameters()
         
 
-# TODO lag også en egen som likner mer på intern Image
+class DCNv3_SW_KA(BaseModule):
+    """Attention Module in Multi-Scale Convolutional Attention Module (MSCA).
+
+    Args:
+        channels (int): The dimension of channels.
+        kernel_sizes (list): The size of attention
+            kernel. Defaults: [5, [1, 7], [1, 11], [1, 21]].
+        paddings (list): The number of
+            corresponding padding value in attention module.
+            Defaults: [2, [0, 3], [0, 5], [0, 10]].
+    """
+
+    def __init__(self,
+                 core_op,
+                 channels,
+                 group,
+                 kernel_size=[5, [1, 7], [1, 11], [1, 21]],
+                 stride=1,
+                 pad=[2, [0, 3], [0, 5], [0, 10]],
+                 dilation=1,
+                 act_layer='GELU',
+                 norm_layer='LN',
+                 post_norm=False,
+                 layer_scale=None,
+                 offset_scale=1.0,
+                 dw_kernel_size=None, # for InternImage-H/G
+                 res_post_norm=False, # for InternImage-H/G
+                 center_feature_scale=False,
+                 strip_conv=1
+                ):
+        super().__init__()
+        self.core_op = getattr(dcnv3, core_op)
+        self.conv0 = nn.Conv2d(
+            channels,
+            channels,
+            kernel_size=kernel_size[0],
+            padding=pad[0],
+            groups=channels)
+        for i, (_kernel_size,
+                padding) in enumerate(zip(kernel_size[1:], pad[1:])):
+            kernel_size_ = [_kernel_size, _kernel_size[::-1]]
+            padding_ = [padding, padding[::-1]]
+            conv_name = [f'conv{i}_1', f'conv{i}_2']
+            for i_kernel, i_pad, i_conv in zip(kernel_size_, padding_,
+                                               conv_name):
+                self.add_module(
+                    i_conv,
+                    self.core_op(
+                        channels=channels,
+                        kernel_size=tuple(i_kernel),
+                        stride=stride,
+                        pad=i_pad,
+                        dilation=dilation,
+                        group=channels,
+                        offset_scale=offset_scale,
+                        act_layer=act_layer,
+                        norm_layer=norm_layer,
+                        dw_kernel_size=dw_kernel_size, # for InternImage-H/G
+                        center_feature_scale=center_feature_scale,
+                        strip_conv=strip_conv
+                        
+                    ))
+        
+        self.conv3 = nn.Conv2d(channels, channels, 1)
+
+    def forward(self, x):
+        """Forward function."""
+        torch.cuda.empty_cache()
+        u = x.clone()
+        u = u.permute(0,3,1,2)
+        x = x.permute(0,3,1,2)
+        attn = self.conv0(x)
+        attn = attn.permute(0,2,3,1)
+        # Multi-Scale Feature extraction
+        attn_0 = self.conv0_1(attn)
+        attn_0 = self.conv0_2(attn_0)
+        
+        attn_1 = self.conv1_1(attn)
+        attn_1 = self.conv1_2(attn_1)
+        
+        attn_2 = self.conv2_1(attn)
+        attn_2 = self.conv2_2(attn_2)
+        
+        attn = attn + attn_0 + attn_1 + attn_2
+        # Channel Mixing
+        print(attn.shape)
+        print("##########################################")
+        attn = attn.permute(0,3,1,2)
+        attn = self.conv3(attn)
+        attn = attn.permute(0,3,1,2)
+        # Convolutional Attention
+        x = attn * u
+
+        return x
+
+    
+    def _reset_parameters(self):#TODO fix
+        
+        self.conv0_1._reset_parameters()
+        self.conv0_2._reset_parameters()
+        self.conv1_1._reset_parameters()
+        self.conv1_2._reset_parameters()
+        self.conv2_1._reset_parameters()
+        self.conv2_2._reset_parameters()
+
+
+
+
+
+
+
